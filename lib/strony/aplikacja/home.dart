2d438a85d0aff24b86d8ab01aba459418627c156
kluchoo <dominikzucho@gmail.com>
2024-12-08 21:12:52 +0100

    Poprawki app baru

--- lib/strony/aplikacja/home.dart
+++ lib/strony/aplikacja/home.dart
@@ -5,6 +5,7 @@ import 'package:do_it/komponenty/botom_navigation_bar.dart';
 import 'package:do_it/providers/home_page_providers.dart';
 import 'package:do_it/strony/aplikacja/zakladki/cytaty.dart';
 import 'package:do_it/strony/aplikacja/zakladki/profil_uzytkownika.dart';
+import 'package:do_it/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -49,91 +50,100 @@ class _HomeState extends ConsumerState<Home> {
     final user = ref.watch(appUserProvider);
     final profileImage = user.photo;
 
-    // Debugowanie
-    if (profileImage != null) {
-      debugPrint('Zdjęcie jest dostępne, długość: ${profileImage}');
-    } else {
-      debugPrint('Brak zdjęcia profilowego');
-    }
-
     return Scaffold(
         resizeToAvoidBottomInset: true,
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(65.0),
-          child: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Color(0xFFFF914d),
-                Color(0xFFFFde59),
-                Color(0xFFFFde59),
-                Color(0xFFFF914d)
-              ], stops: [
-                0,
-                0.4,
-                0.6,
-                1
-              ])),
-              child: Stack(
-                children: [
-                  AppBar(
-                    backgroundColor: Colors.transparent,
-                  ),
-                  Center(
-                      child: Text(
-                    title,
-                    style: GoogleFonts.openSans(
-                      fontSize: 22,
-                      letterSpacing: 1,
-                      color: const Color.fromARGB(255, 0, 0, 0),
-                      fontWeight: FontWeight.w800,
-                    ),
-                  )),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ProfileSettingsPage()));
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.height * 0.1,
-                          height: MediaQuery.of(context).size.height * 0.1,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: ClipOval(
-                            child: profileImage != null
-                                ? Image.memory(
-                                    profileImage,
-                                    fit: BoxFit.cover,
-                                    scale: 0.5,
-                                    errorBuilder: (context, error, stackTrace) {
-                                      debugPrint(
-                                          'Błąd ładowania zdjęcia: $error');
-                                      return const Icon(
-                                        Icons.person,
-                                        size: 65,
-                                        color: Colors.white,
-                                      );
-                                    },
-                                  )
-                                : const Icon(
-                                    Icons.person,
-                                    size: 65,
-                                    color: Colors.white,
-                                  ),
+          child: Stack(children: [
+            Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color(0xFFFF914d),
+                  Color(0xFFFFde59),
+                  Color(0xFFFFde59),
+                  Color(0xFFFF914d)
+                ], stops: [
+                  0,
+                  0.4,
+                  0.6,
+                  1
+                ])),
+                child: Stack(
+                  children: [
+                    OverflowBox(
+                      maxWidth: 135,
+                      maxHeight: 135,
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryAccent,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(100),
                           ),
                         ),
                       ),
-                    ],
-                  )
-                ],
-              )),
+                    ),
+                    AppBar(
+                      backgroundColor: Colors.transparent,
+                      centerTitle: false,
+                      title: Text(
+                        title,
+                        style: GoogleFonts.openSans(
+                          fontSize: 22,
+                          letterSpacing: 1,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ProfileSettingsPage()));
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.height * 0.1,
+                            height: MediaQuery.of(context).size.height * 0.4,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: ClipOval(
+                              child: profileImage != null
+                                  ? Image.memory(
+                                      profileImage,
+                                      fit: BoxFit.cover,
+                                      scale: 0.5,
+                                      errorBuilder:
+                                          (context, error, stackTrace) {
+                                        debugPrint(
+                                            'Błąd ładowania zdjęcia: $error');
+                                        return const Icon(
+                                          Icons.person,
+                                          size: 65,
+                                          color: Colors.white,
+                                        );
+                                      },
+                                    )
+                                  : const Icon(
+                                      Icons.person,
+                                      size: 65,
+                                      color: Colors.white,
+                                    ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+          ]),
         ),
         body: MainAppBackground(Stack(
           children: [
