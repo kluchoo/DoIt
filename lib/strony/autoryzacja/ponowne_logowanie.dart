305e3b8b2cc6d4a00d4d5515c97129fbd2644690
kluchoo <dominikzucho@gmail.com>
2024-12-17 00:30:15 +0100

    Efekt finalny

--- lib/strony/autoryzacja/ponowne_logowanie.dart
+++ lib/strony/autoryzacja/ponowne_logowanie.dart
@@ -1,7 +1,11 @@
+import 'package:do_it/komponenty/styled_button.dart';
 import 'package:do_it/komponenty/text.dart';
 import 'package:do_it/providers/home_page_providers.dart';
 import 'package:do_it/services/auth_service.dart';
+import 'package:do_it/services/local_auth_service.dart';
+import 'package:do_it/strony/aplikacja/home.dart';
 import 'package:do_it/strony/autoryzacja/welcome.dart';
+import 'package:do_it/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -40,6 +44,12 @@ class _ReLogInState extends ConsumerState<ReLogIn> {
   Widget build(BuildContext context) {
     final user = ref.watch(appUserProvider);
     debugPrint('User: ${user.photo}');
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController _passwordController = TextEditingController();
+
+    String? _errorFeedback;
+    bool authenticated = false;
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -90,6 +100,118 @@ class _ReLogInState extends ConsumerState<ReLogIn> {
                       color: const Color.fromARGB(255, 78, 174, 253)),
                   const StylizowanyText('!'),
                 ],
+              ),
+              const SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: AppColors.titleColor,
+                          prefixIcon: const Icon(
+                            Icons.key,
+                            color: Colors.black,
+                          ),
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(50)),
+                              borderSide: BorderSide(
+                                  color: AppColors.primaryColor, width: 2))),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Podaj hasło';
+                        }
+                        if (value.length < 8) {
+                          return 'Nieprawidłowe hasło';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    StyledButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            _errorFeedback = null;
+                          });
+
+                          final email = user.email;
+                          final password = _passwordController.text.trim();
+
+                          final signedInUser =
+                              await AuthService.signIn(email, password);
+
+                          // error feedback
+                          if (signedInUser == null) {
+                            setState(() {
+                              _errorFeedback = 'Nieprawidłowe dane logowania';
+                            });
+                          } else {
+                            ref.read(appUserProvider).uid = signedInUser.uid;
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (ctx) => const Home()));
+
+                            // navigate to home on success
+                          }
+                        }
+                      },
+                      child: const StyledButtonText('Zaloguj się'),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text('lub'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final authenticate = await LocalAuth.authenticate();
+                  setState(() {
+                    authenticated = authenticate;
+                  });
+                  if (authenticated) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => const Home()));
+                  }
+                },
+                child: Icon(
+                  Icons.fingerprint,
+                  size: 75,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 45.0,
               ),
               const StylizowanyText('Chcesz zmienić konto?'),
               TextButton(
